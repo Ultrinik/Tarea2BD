195a375204fb633bc8897c58795449c88d167bf8
Nicolas Castro <nicolascastroa@live.com>
2021-06-10 00:32:25 -0400

    gitignore?

--- db_config.php
+++ db_config.php
@@ -1,6 +1,6 @@
 <?php
 /* Detalles de la conexión */
-$conn_string = "host=localhost port=5432 dbname= user= password=";
+$conn_string = "host=localhost port=5432 dbname=postgres user=postgres password=Ultrinik100";
 // Recuerde reemplazar "<contraseña>" por su contraseña y "<nombre_db>" por el nombre de su BD. No se incluyen los "<>".
 // Establecemos una conexión con el servidor postgresSQL
 $dbconn = pg_connect($conn_string);
