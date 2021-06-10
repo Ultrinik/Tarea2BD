fba6fc98be24badd91666bcf445e33bf883c41df
Nicolas Castro <nicolascastroa@live.com>
2021-06-10 00:23:20 -0400

    Actualizacion en Profile

--- sesion/valida_login.php
+++ sesion/valida_login.php
@@ -6,35 +6,27 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $sql = 'SELECT * FROM usuario WHERE correo = $1';
     $result = pg_query_params($dbconn, $sql, array($correo));
 	
-    if( pg_num_rows($result) == 1 ) {
-        while($row = pg_fetch_assoc($result)) {
-            if(password_verify($contrasena, $row['contraseña'])){
-			#if($contrasena == $row['contraseña']){
+    while($row = pg_fetch_assoc($result)) {
+        if(password_verify($contrasena, $row['contraseña'])){
+        #if($contrasena == $row['contraseña']){
 
-                session_start();
-                $_SESSION["usuario"] = $row['nombre'] . ' ' . $row['apellido'];
-                $_SESSION["correo"]= $row['correo'];
-                $_SESSION["pais"]= $row['pais'];
-                $_SESSION["fecha_ingreso"]= $row['fecha_ingreso'];
-                pg_close($dbconn);
-        
-                if($row['administrador']){
-                    header('Location: '.'/admin/users/all.html');
-                    exit();
-                } else {
-                    header('Location: '.'/user/profile.php');
-                    exit();
-                }
-                
-                
-            } else {
-                echo "<br> Contraseña incorrecta";
-                pg_close($dbconn);
-            }
+            session_start();
+            $_SESSION["usuario"] = $row['nombre'] . ' ' . $row['apellido'];
+            $_SESSION["correo"]= $row['correo'];
+            $_SESSION["pais"]= $row['pais'];
+            $_SESSION["fecha_registro"]= $row['fecha_registro'];
+            pg_close($dbconn);
+    
 
+            header('Location: '.'/user/profile.html');
+            exit();
+            
+            
+        } else {
+            echo "<br> Contraseña incorrecta";
+            pg_close($dbconn);
         }
-    } else {
-        echo "<br> Multiples instancias del correo en la DB";
+
     }
     
 }
--- user/profile.php
+++ user/profile.php
@@ -1,21 +1,14 @@
-<?php session_start();?>
-<?php include '../include/navbar.html'; ?>
-    <div class='container-fluid'>
-        <div class="row p-3">
-            <h1>Mi Perfil</h1>
-        </div>
-        <div class="row p-3">
-            <div class="col">
-                <!-- Mostrar aquí los datos del usuario de la sesión -->
-                <div class="container shadow-lg rounded m-auto p-5">
-                    <p>Nombre Completo: <?= $_SESSION["usuario"] ?></p>
-                    <p>Correo: <?= $_SESSION["email"] ?></p>
-                    <p>País: <?= $_SESSION["pais"] ?></p>
-                    <p>Fecha de Ingreso: <?= $_SESSION["fecha_ingreso"] ?></p>
-                </div>
-            </div>
-        </div>
-    </div>
-</body>
+<?php session_start();
+include $_SERVER['DOCUMENT_ROOT'].'/db_config.php';
 
-</html>
+$nombre = $_SESSION["usuario"];
+$correo = $_SESSION["correo"];
+
+$paisId = $_SESSION["pais"];
+$sql = 'SELECT nombre FROM pais WHERE cod_pais = $1';
+$result = pg_query_params($dbconn, $sql, array($paisId));
+$row = pg_fetch_row($result);
+$pais = $row[0];
+
+$fecha_registro = $_SESSION["fecha_registro"];
+?>
