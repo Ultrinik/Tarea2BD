8ef07441d04bc35813f899ac493e51bf57d93f9d
lucas <lucas.antoncich@sansano.usm.cl>
2021-06-13 22:44:33 -0400

    calculo total

--- user/wallet.php
+++ user/wallet.php
@@ -24,6 +24,26 @@ if(isset($_SESSION['id'])){
     $result = pg_query_params($dbconn, $sql, array($_SESSION['id']));
     $num_billeteras = pg_num_rows($result);
 
+    $sql = 'SELECT nombre, apellido, SUM(valor*balance) AS Valor_Total
+    FROM(SELECT ultimo_valor.id_moneda, valor
+             FROM (
+            SELECT id_moneda, max(fecha)
+            FROM precio_moneda
+            GROUP BY id_moneda) AS ultimo_valor 
+            LEFT JOIN precio_moneda ON ultimo_valor.max = precio_moneda.fecha 
+            AND ultimo_valor.id_moneda = precio_moneda.id_moneda)
+            AS valor_monedas
+        RIGHT JOIN 
+        (SELECT *
+             FROM usuario_tiene_moneda LEFT JOIN usuario 
+             ON usuario_tiene_moneda.id_usuario = usuario.id
+             WHERE usuario.id = $1) AS moneda_usuario
+        ON moneda_usuario.id_moneda = valor_monedas.id_moneda
+    GROUP BY
+        nombre, apellido';
+    
+    $result1 = pg_query_params($dbconn, $sql, array($_SESSION['id']));
+    $row1 = pg_fetch_row($result1);
     pg_close($dbconn);
 }
 else{
