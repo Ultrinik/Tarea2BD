3a825ef5229f59a70283c2fe208613e82552af0d
Nicolas Castro <nicolascastroa@live.com>
2021-07-06 19:26:20 -0400

    Primer metodo crud???

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import jsonify
 from config import config
 from models import db
-from models import User
+from models import *
 from flask import request
 
 def create_app(enviroment):
@@ -18,7 +18,16 @@ enviroment = config['development']
 app = create_app(enviroment)
 
 #USUARIO ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+@app.route('/api/usuario/', methods=['POST'])
+def crear_usuario():
+	json = request.get_json(force=True)
 
+	if json.get('nombre') is None:
+		return jsonify({'message': 'El formato está mal'}), 400
+
+	usario = Usuario.create(json['nombre'],json['apellido'],json['correo'],json['contraseña'],json['fecha_registro'])
+
+	return jsonify({'usario': usario.json() })
 
 if __name__ == '__main__':
 	app.run(debug=True)
