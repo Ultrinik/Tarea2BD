6e976b318294ead15553c746a63886dc80f5b901
Nicolas Castro <nicolascastroa@live.com>
2021-07-06 23:41:34 -0400

    mini formalidades

--- poblarBDT1.py
+++ poblarBDT1.py
@@ -2,12 +2,12 @@ from datetime import date, datetime, timedelta
 import random as r
 
 #Librerías para PostgreSQL
-
+import pg
 import psycopg2
 
-usuario = "nombre usuario"
-nombreDB = "nombre bd"
-contraseña = "contraseña"
+usuario = "Escribir usuario"
+nombreDB = "Escribir nombre BD"
+contraseña = "Escribir contraseña"
 
 connection = psycopg2.connect(
    host="localhost",
