0e293f1c31d5a0d1826113e5e2b48bbc2436cd39
Nicolas Castro <nicolascastroa@live.com>
2021-07-22 19:41:36 -0400

    validarSesion ahora tiene la url en una variable

--- extras/CRUD/validar_sesion.php
+++ extras/CRUD/validar_sesion.php
@@ -3,6 +3,10 @@
 if(isset($_SESSION['id'])){
     $sesionActiva = 1;
     $admin = $_SESSION['administrador'];
+    $url.= $_SERVER['HTTP_HOST'];   
+    
+    // Append the requested resource location to the URL   
+    $url.= $_SERVER['REQUEST_URI'];    
 }
 else{
     $sesionActiva = 0;
