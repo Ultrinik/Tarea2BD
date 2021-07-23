147dbce2cb27fcdf97d373d3051e1e163c4717a9
Nicolas Castro <nicolascastroa@live.com>
2021-07-22 20:25:16 -0400

    No se que estoy haciendo, ahhh

--- extras/CRUD/validar_sesion.php
+++ extras/CRUD/validar_sesion.php
@@ -7,7 +7,7 @@ if(isset($_SESSION['id'])){
     if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')   
          $url = "https://";   
     else  
-         $url = "http://";   
+         $url = "http://";
     $url.= $_SERVER['HTTP_HOST'];   
     $url.= $_SERVER['REQUEST_URI'];    
 }
